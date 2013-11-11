93f80c0fc3e0cd59e1f99123cf2df5ccbc2f7c41
Christopher Martin <ch.martin@gmail.com>
2013-11-10 21:21:44 -0500

    tmp

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "org.chris_martin.sortimg"
 version := "1.0"
 
 scalaVersion := "2.9.1"
+
+libraryDependencies += "com.github.scala-incubator.io" %% "scala-io-file" % "0.2.0"
--- src/main/scala/Sortimg.scala
+++ src/main/scala/Sortimg.scala
@@ -1,5 +1,56 @@
 package org.chris_martin.sortimg
 
+import scalax.io.StandardOpenOption._
+
 object Sortimg {
-  def main(args: Array[String]) { println("Hi!") }
+
+  case class Point(x:Double, y:Double) {
+    def binary_+(that:Point) = Point(x+that.x, y+that.y)
+    def binary_-(that:Point) = Point(x-that.x, y-that.y)
+    def binary_*(scalar:Double) = Point(x*scalar, y*scalar)
+    def binary_/(scalar:Double) = Point(x/scalar, y/scalar)
+  }
+
+  trait BezierCommand {
+    def pathStepString:String
+  }
+
+  case class M(p:Point) extends BezierCommand {
+    def pathStepString = "M%f,%f".format(p.x, p.y)
+  }
+
+  case class C(p:Point, from:Point, to:Point) extends BezierCommand {
+    def pathStepString = "M%f,%f".format(p.x, p.y)
+  }
+
+  case class Path() {
+
+  }
+
+  val arr = Seq(4, 5, 9)
+
+  val a = Seq(
+    <style type="text/css"><![CDATA[
+      .Lane { fill: none; stroke-width: 5; stroke-linecap: round; stroke-linejoin: round; }
+    ]]></style>
+    <rect x="0" y="0" width="700" height="700" style="fill:white;" />
+    <path d="M300,300 400,500 C400,400 420,450 460,460 S 400,400 500,500" stroke="red" class="Lane"/>
+  )
+
+  def main(args: Array[String]) {
+
+    scalax.file.Path("/tmp/sortimg.svg").open(Seq(CreateFull, Write, Truncate)) { out =>
+
+      out.append("""<?xml version="1.0" encoding="iso-8859-1"?>
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20001102//EN"
+        "http://www.w3.org/TR/2000/CR-SVG-20001102/DTD/svg-20001102.dtd">""")
+
+      out.append(
+        <svg width="700" height="700">
+          { a }
+        </svg>.toString()
+      )
+    }
+  }
+
 }
